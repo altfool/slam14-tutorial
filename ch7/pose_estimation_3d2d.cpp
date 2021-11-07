8495dde743da3ee431f91ad45e878c5afd777408
Zack <zongmiy@clemson.edu>
2021-11-06 22:13:44 -0400

    complete pose estimation 3d2d by g2o

--- ch7/pose_estimation_3d2d.cpp
+++ ch7/pose_estimation_3d2d.cpp
@@ -29,6 +29,9 @@ typedef std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d>> 
 void bundleAdjustmentGaussNewton(const VecVector3d &points_3d, const VecVector2d &points_2d, const cv::Mat &K,
                                  Sophus::SE3d &pose);
 
+void bundleAdjustmentG2O(const VecVector3d &points_3d, const VecVector2d &points_2d, const cv::Mat &K,
+                         Sophus::SE3d &pose);
+
 int main(int argc, char **argv) {
     if (argc != 5) {
         std::cout << "usage: pose_estimation_3d2d img1 img2 depth1 depth2" << std::endl;
@@ -86,6 +89,15 @@ int main(int argc, char **argv) {
     t2 = std::chrono::steady_clock::now();
     time_used = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
     std::cout << "solve pnp by gauss newton cost time: " << time_used.count() << " seconds. " << std::endl;
+
+    std::cout << "calling bundle adjustment by g2o" << std::endl;
+    Sophus::SE3d pose_g2o;
+    t1 = std::chrono::steady_clock::now();
+    bundleAdjustmentG2O(pts_3d_eigen, pts_2d_eigen, K, pose_g2o);
+    t2 = std::chrono::steady_clock::now();
+    time_used = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+    std::cout << "solve pnp by g2o cost time: " << time_used.count() << " seconds. " << std::endl;
+    return 0;
 }
 
 void find_feature_matches(const cv::Mat &img_1, const cv::Mat &img_2, std::vector<cv::KeyPoint> &keypoints_1,
@@ -176,4 +188,108 @@ void bundleAdjustmentGaussNewton(const VecVector3d &points_3d, const VecVector2d
         }
     }
     std::cout << "pose by g-n: \n" << pose.matrix() << std::endl;
+}
+
+/// vertex and edges in g2o BA
+class VertexPose : public g2o::BaseVertex<6, Sophus::SE3d> {
+public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+
+    virtual void setToOriginImpl() override {
+        _estimate = Sophus::SE3d();
+    }
+
+    /// left multiplication on SE3
+    virtual void oplusImpl(const double *update) override {
+        Eigen::Matrix<double, 6, 1> update_eigen;
+        update_eigen << update[0], update[1], update[2], update[3], update[4], update[5];
+        _estimate = Sophus::SE3d::exp(update_eigen) * _estimate;
+    }
+
+    virtual bool read(std::istream &in) override {}
+
+    virtual bool write(std::ostream &out) const override {}
+};
+
+class EdgeProjection : public g2o::BaseUnaryEdge<2, Eigen::Vector2d, VertexPose> {
+public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+
+    EdgeProjection(const Eigen::Vector3d &pos, const Eigen::Matrix3d &K) : _pos3d(pos), _K(K) {}
+
+    virtual void computeError() override {
+        const VertexPose *v = static_cast<VertexPose *> (_vertices[0]);
+        Sophus::SE3d T = v->estimate();
+        Eigen::Vector3d pos_pixel = _K * (T * _pos3d);
+        pos_pixel /= pos_pixel[2];
+        _error = _measurement - pos_pixel.head<2>();
+    }
+
+    virtual void linearizeOplus() override {
+        const VertexPose *v = static_cast<VertexPose *> (_vertices[0]);
+        Sophus::SE3d T = v->estimate();
+        Eigen::Vector3d pos_cam = T * _pos3d;
+        double fx = _K(0, 0);
+        double fy = _K(1, 1);
+        double cx = _K(0, 2);
+        double cy = _K(1, 2);
+        double X = pos_cam[0];
+        double Y = pos_cam[1];
+        double Z = pos_cam[2];
+        double Z2 = Z * Z;
+        _jacobianOplusXi << -fx / Z, 0, fx * X / Z2, fx * X * Y / Z2, -fx - fx * X * X / Z2, fx * Y / Z,
+                0, -fy / Z, fy * Y / Z2, fy + fy * Y * Y / Z2, -fy * X * Y / Z2, -fy * X / Z;
+    }
+    virtual bool read(std::istream &in) override {}
+    virtual bool write(std::ostream &out) const override {}
+
+private:
+    Eigen::Vector3d _pos3d;
+    Eigen::Matrix3d _K;
+};
+
+void bundleAdjustmentG2O(const VecVector3d &points_3d, const VecVector2d &points_2d, const cv::Mat &K,
+                         Sophus::SE3d &pose) {
+    typedef g2o::BlockSolver<g2o::BlockSolverTraits<6, 3>> BlockSolverType;  // pose is6, landmark is 3;
+    typedef g2o::LinearSolverDense<BlockSolverType::PoseMatrixType> LinearSolverType;   // build linear solver
+    auto solver = new g2o::OptimizationAlgorithmGaussNewton(
+            g2o::make_unique<BlockSolverType>(g2o::make_unique<LinearSolverType>()));
+    g2o::SparseOptimizer optimizer;
+    optimizer.setAlgorithm(solver);
+    optimizer.setVerbose(true);
+
+    //vertex
+    VertexPose *vertex_pose = new VertexPose();
+    vertex_pose->setId(0);
+    vertex_pose->setEstimate(Sophus::SE3d());
+    optimizer.addVertex(vertex_pose);
+
+    // K
+    Eigen::Matrix3d K_eigen;
+    K_eigen << K.at<double>(0, 0), K.at<double>(0, 1), K.at<double>(0, 2),
+            K.at<double>(1, 0), K.at<double>(1, 1), K.at<double>(1, 2),
+            K.at<double>(2, 0), K.at<double>(2, 1), K.at<double>(2, 2);
+
+    //edges
+    int index = 1;
+    for (size_t i = 0; i < points_2d.size(); i++) {
+        auto p2d = points_2d[i];
+        auto p3d = points_3d[i];
+        EdgeProjection *edge = new EdgeProjection(p3d, K_eigen);
+        edge->setId(index);
+        edge->setVertex(0, vertex_pose);
+        edge->setMeasurement(p2d);
+        edge->setInformation(Eigen::Matrix2d::Identity());
+        optimizer.addEdge(edge);
+        index++;
+    }
+    std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+    optimizer.setVerbose(true);
+    optimizer.initializeOptimization();
+    optimizer.optimize(10);
+    std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+    std::chrono::duration<double> time_used = std::chrono::duration_cast<std::chrono::duration<double>>(t2-t1);
+    std::cout << "optimization costs time: " << time_used.count() << " seconds. " << std::endl;
+    std::cout << "pose estimated by g2o = " << std::endl << vertex_pose->estimate().matrix() << std::endl;
+    pose = vertex_pose->estimate();
 }
